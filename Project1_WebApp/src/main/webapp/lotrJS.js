0da941929b1345553888c68947ff288597f814d9
Dillon Mason <d14mason@yahoo.com>
2019-10-16 14:58:27 -0400

    Project 1

--- Project1_WebApp/src/main/webapp/lotrJS.js
+++ Project1_WebApp/src/main/webapp/lotrJS.js
@@ -1,5 +1,5 @@
 let user;
-let server = "http://ec2-52-14-82-74.us-east-2.compute.amazonaws.com:8080/Project1_WebApp";
+let server = "http://ec2-18-221-114-64.us-east-2.compute.amazonaws.com:8080/Project1_WebApp";
 
 //LoginPage JS
     function login(){
